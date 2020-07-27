4032453aa11c0d3bf51c51581036da053cf03107
Mr. Senko <atodorov@mrsenko.com>
2020-07-27 15:57:37 +0300

    Add header & footer in base.html template
    - moving files & adjusting paths as necessary
    - remove the local copy of FontAwesome b/c the new styles use
      LineAwesome

---
+++ theme/static/scripts/custom-vanilla-async.js
@@ -0,0 +1,34 @@
+// This is the Async JS directory.
+// This loads asynchronous to the page.
+// Everything that's not a MUST LOAD for
+// above the fold should come here as to make
+// the site load faster.
+
+// EVENT LISTENERS
+
+// FUNCTIONS
+function toggle(objectId, toggleClass) {
+    var toggleObject = document.getElementById(objectId);
+    if (toggleObject.classList.contains(toggleClass)) {
+        toggleObject.classList.remove(toggleClass);
+    } else {
+        toggleObject.classList.add(toggleClass);
+    }
+}
+
+function doVideo(videoId, open) {
+    var player = document.getElementById("vp-frame");
+    var vpOverlay = document.getElementById("vp-overlay");
+    if(open){
+        player.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&hd=1&showinfo=0&enablejsapi=1";
+        vpOverlay.classList.add("vp-visible");
+    } else {
+        stopVideo();
+        vpOverlay.classList.remove("vp-visible");
+    }
+}
+
+function stopVideo(){
+    var video = document.getElementById("vp-frame");
+    video.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+}
