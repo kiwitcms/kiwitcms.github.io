f0a097a40a0c12a1a7133ac7216f72b0963f0e6f
Mr. Senko <atodorov@mrsenko.com>
2020-11-08 23:29:58 +0200

    Revert "Add static files for SSL validation"
    This reverts commit 4bb73f94f594e4cf9643258e85776f258f325235.

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,7 +30,7 @@ CATEGORIES_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 
 STATIC_PATHS = ['google/google8b349b847d57319a.html', 'CNAME', 'images/',
-                'scripts/', 'README', '.well-known/']
+                'scripts/', 'README']
 EXTRA_PATH_METADATA = {
     'google/google8b349b847d57319a.html': {'path': 'google8b349b847d57319a.html'},
 }
